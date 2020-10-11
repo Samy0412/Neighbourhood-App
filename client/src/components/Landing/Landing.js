051a0f3f9d2cd7fc8574b9f9b89e003c3d6671e7
Samy0412 <samantha.gadet@gmail.com>
2020-10-10 20:16:32 -0400

    improvements landing page

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -5,20 +5,53 @@ import "../../styles.scss";
 function Landing(props) {
   return (
     <div>
-      <div className="landing-container">
-        <div className="landing-image">
-          <img src="/images/banner-dark-blue.png" alt="CupOSugah"></img>
+      <header className="landing-header">
+        <div class="logo-container">
+          <img src="./logo.png" alt="logo" />
+          <h4 class="logo">CupOSugah</h4>
         </div>
-        <h2 className="landing-title">Your neighbourhood social network!</h2>
-        <div className="button-container">
-          <Button variant="contained" color="primary" href="/login">
+        <nav>
+          <ul class="nav-links">
+            <li>
+              <a class="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li>
+              <a class="nav-link" href="#">
+                Team
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div class="login-container">
+          <Button className="login-button" variant="contained" href="/login">
             LOGIN
           </Button>
-          <Button variant="contained" color="primary" href="/register">
-            REGISTER
-          </Button>
         </div>
-      </div>
+      </header>
+      <main>
+        <section class="presentation">
+          <div class="introduction">
+            <div class="intro-text">
+              <h1>Welcome to your neighbourhood!</h1>
+              <p>Come and join the community blabalalalalalalalaalalaal.</p>
+            </div>
+            <div class="sign-in-button">
+              <Button
+                className="login-button"
+                variant="contained"
+                href="/register"
+              >
+                Sign In
+              </Button>
+            </div>
+          </div>
+          <div class="cover">
+            <img src="./images/neighbourhood.png" alt="neighbourhood" />
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
